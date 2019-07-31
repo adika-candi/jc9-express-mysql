83cce527be4f64600db6bea6fd5cd0417afa515c
Adika Tandiono Candi <adika.t.c@gmail.com>
2019-07-31 10:41:07 +0700

    add get /

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(router)
 app.use(taskRouter)
 
+app.get("/",(req,res)=>{
+    res.send(`<h1>WELCOME</h1>`)
+})
+
 app.listen(port,()=>{
     console.log("running di port :"+port)
 })
